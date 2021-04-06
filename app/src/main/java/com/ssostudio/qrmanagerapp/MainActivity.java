745946ed2ec09204fd59de462bcd4c74c0714120
sojaehong <woghd4545@gmail.com>
2021-04-07 00:46:53 +0900

    - 스캔 기능 제작 완료 - 스캐너 뷰 커스텀 제작중 - 스캔 완료 후 작업 제작중

--- app/src/main/java/com/ssostudio/qrmanagerapp/MainActivity.java
+++ app/src/main/java/com/ssostudio/qrmanagerapp/MainActivity.java
@@ -1,14 +1,71 @@
 package com.ssostudio.qrmanagerapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.ssostudio.qrmanagerapp.scanner.QRScanActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MaterialButton qrScanBtn;
+    private IntentIntegrator integrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setQrScanBtn();
+    }
+
+    private void setQrScanBtn() {
+        qrScanBtn = findViewById(R.id.qr_scan_button);
+        qrScanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.qr_scan_button:
+                onQRScanBtnClick();
+                break;
+        }
+    }
+
+    private void onQRScanBtnClick() {
+        integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(QRScanActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setOrientationLocked(false);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                if (integrator != null){
+                    integrator.addExtra("result", result.getContents());
+                    integrator.initiateScan();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/ssostudio/qrmanagerapp/scanner/AnyOrientationCaptureActivity.java
@@ -0,0 +1,8 @@
+package com.ssostudio.qrmanagerapp.scanner;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+// 스캔화면의 세로형을 위해 제작
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+
+}
